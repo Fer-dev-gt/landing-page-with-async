be9935e6cc5573a78ea1154379471f402550b1e3
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-04-29 13:01:51 -0600

    Even more links and interactivity :D

--- src/assets/main.js
+++ src/assets/main.js
@@ -25,9 +25,9 @@ const content = null || document.getElementById('content');                     
     // Voy a crear un "template" que va iterar cada uno de los elementos que esten en la respuesta de "videos" y va a crear una estructura HTML para agregar la información que yo deseo a la página web y mostrar sus elementos
     let view = `                                                                    <!-- El template (estuctura HTML lo guardare´en la variable "view") -->
     ${videos.items.map(video => `                                                   <!-- Accedo a los "items" de la respuesta en "videos" y utilizo el método "map" para que me retorne un nuevo "array" pero con la transformación que le estoy aplicando (En este caso el "template" HTML) a los elementos del array que obtuve, ".map()" me va a retornar un nuevo valor, los guardo en la variable "video" e utilizo de nuevo una Arrow Function-->
-      <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
+      <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">    <!-- Usando 'target="_blank" le indico al HTML y al enlace que vaya a la página en una nueva pestaña'-->
         <div class="group relative">        
-          <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-lg overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-lg overflow-hidden group-hover:opacity-80 lg:aspect-none">
             <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">                          <!-- Coloco como fuente de la imagen del video la dirreción que esta definida en mi objeto JSON en "videos" "video.snippet.thumbnails.high.url" y hago algo similar en la descripción del video "video.snippet.description"-->
           </div>
           <div class="mt-4 flex justify-between" style="text-shadow: 1px 1px 4px yellow, 0 0 2em yellow, 0 0 0.4em yellow;" >
